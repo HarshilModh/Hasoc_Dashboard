a46b80a31835e9a323e2409e8bd2d9f1813a963e
HarshilModh <harshilmodh77@gmail.com>
2021-08-07 23:04:48 +0530

    /

--- Hasoc.js
+++ Hasoc.js
@@ -404,3 +404,58 @@ function team_data_timestamp_desc(){
         }
         });
 }
+function leaderboard(){
+    $.ajax({
+        type: 'POST',
+        url: "https://hasocsubmission.el.r.appspot.com/dashboard/team_data/timestamp_desc",
+       headers:{
+            "x-access-token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0ZWFtX25hbWUiOiJQYXZhbiJ9.iy6eVZITa6mQqvjP4EPw6cXkrVA7h8_nPlch2B0mb10",
+           'content-type':'application/json'
+       },
+        data:JSON.stringify( {
+            "team_name": "Pavan"
+        }),
+        success: function (result){
+            // var request=new XMLHttpRequest();
+            // token=request.getResponseHeader("x-mstr-authtoken")
+            //console.log(result.token)
+            //var token=result.token
+            // console.log(token);
+            //document.cookie=token;
+            console.log(result);
+            for(var i=0;i<result.length;i++){
+
+                var table = document.getElementById("team_data").getElementsByTagName('tbody')[0]
+                var newRow = table.insertRow(table.length)
+                cell1 = newRow.insertCell(0)
+                cell1.innerHTML = result[i].timestamp
+            
+                cell2 = newRow.insertCell(1)
+                cell2.innerHTML = result[i].submission_name
+            
+                cell3 = newRow.insertCell(2)
+                cell3.innerHTML =result[i].task_name
+            
+                cell4 = newRow.insertCell(3)
+                cell4.innerHTML =result[i].f1_score
+            
+                cell5 = newRow.insertCell(4)
+                var data='<td><button class="btn btn-info" data-toggle="modal" data-target="#myModal3" >Deatils</button></td>'
+                cell5.innerHTML =data
+                
+                cell6 = newRow.insertCell(5)
+                cell6.innerHTML =' <td><button class="btn btn-info"><a href="Leader_Board.html" style="text-decoration: none; color: white;">Rank</a> </button></td>'
+            }
+        
+            // var res=result[1].description;
+            // console.log(result[1].description);
+            // console.log(result[1].task_name);
+            // console.log(result[1].f1_score);
+            // console.log(result[1].submission_name);
+            
+        
+            //alert( document.cookie)
+           
+        }
+        });
+}
